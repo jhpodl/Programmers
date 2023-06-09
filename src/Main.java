d4b51f4aa7c67e5d48ba23df76a4dfa98c422717
정재우 <jjwoo@corealism.com>
2023-06-09 13:49:21 +0900

    [226] n^2 배열 자르기 실패 : 메모리초과

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,35 @@
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int[] elements = {7,9,1,1,4};
-        System.out.println(sol.solution(elements));
+        String testCase1=Arrays.toString(sol.solution(3,2,5));
+        String testCase2=Arrays.toString(sol.solution(4,7,14));
+        System.out.println(testCase1);
+        System.out.println(testCase2);
     }
 
 }
 class Solution {
-    public int solution(int[] elements) {
-        Set<Integer> resultSet = new HashSet<>();
-        stepByLength(elements, resultSet);
-        return resultSet.size();
-    }
+    public int[] solution(int n, long left, long right) {
+        List<Integer> linear = new ArrayList<Integer>();
+        for(int i=1; i<=n ; i++){
+            for(int j=1; j<=i ; j++){
+                linear.add(i);
+            }
+            for(int j=i+1; j<=n ; j++){
+                linear.add(j);
+            }
+        }
+        Integer[] subInteger = linear.subList((int)left,(int)right+1).toArray(new Integer[0]);
+        int[] answer = Arrays.stream(subInteger).mapToInt(Integer::intValue).toArray();
 
-    private static void stepByLength(int[] elements, Set<Integer> resultSet) {
-        for (int r = 1; r <= elements.length; r++) {
-            combineConsecutive(elements, resultSet, new ArrayList<>(), r);
-        }
-    }
-    static void combineConsecutive(int[] elements, Set<Integer> resultSet, List<Integer> tempList, int r) {
-        for(int i=0; i<elements.length; i++){
-            addUntilLength(elements, tempList, r, i);
-            resultSet.add(tempList.stream().mapToInt(Integer::intValue).sum());
-            tempList.clear();
-        }
-    }
-    private static void addUntilLength(int[] elements, List<Integer> tempList, int r, int i) {
-        for(int j = 0; j< r; j++){
-            tempList.add(elements[(i +j)% elements.length]);
-        }
+        return answer;
     }
 }
+//참고용
+//List<Integer> linear = Arrays.asList(1, 2, 3, 4, 5);
+//List<Integer> linear2 = new ArrayList<>(circular);  // 선형 수열을 생성하고 원형 수열의 원소를 복사
+//linear2.addAll(circular);
