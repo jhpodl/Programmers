f70827cbcdfc44c5882fec66d4505c525159faa2
정재우 <jjwoo@corealism.com>
2023-06-09 09:46:22 +0900

    [225] Refactoring (low indent)

--- src/Main.java
+++ src/Main.java
@@ -9,24 +9,28 @@ public class Main {
     }
 
 }
-
 class Solution {
     public int solution(int[] elements) {
         Set<Integer> resultSet = new HashSet<>();
-        for (int r = 1; r <= elements.length; r++) {
-            combineConsecutive(elements, resultSet, new ArrayList<>(), r);
-        }
+        stepByLength(elements, resultSet);
         return resultSet.size();
     }
 
+    private static void stepByLength(int[] elements, Set<Integer> resultSet) {
+        for (int r = 1; r <= elements.length; r++) {
+            combineConsecutive(elements, resultSet, new ArrayList<>(), r);
+        }
+    }
     static void combineConsecutive(int[] elements, Set<Integer> resultSet, List<Integer> tempList, int r) {
-
         for(int i=0; i<elements.length; i++){
-            for(int j=0; j<r; j++){
-                tempList.add(elements[(i+j)%elements.length]);
-            }
+            addUntilLength(elements, tempList, r, i);
             resultSet.add(tempList.stream().mapToInt(Integer::intValue).sum());
             tempList.clear();
+        }
+    }
+    private static void addUntilLength(int[] elements, List<Integer> tempList, int r, int i) {
+        for(int j = 0; j< r; j++){
+            tempList.add(elements[(i +j)% elements.length]);
         }
     }
 }
